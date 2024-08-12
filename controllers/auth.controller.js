a271d97c8d56418d8288405e4f091145491b5b78
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-12 23:59:58 +0200

    useless comment

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler');
 
 const adminLogin = asyncHandler(async(req, res) => {
     console.log('req.body', req.body);
-    // res.send('Hello from API');
+
 });
 
 module.exports = { adminLogin }
