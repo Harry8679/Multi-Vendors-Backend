2c261f048ad8ac7510c9f0ff2e3d6beabfdb677c
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-16 11:26:56 +0200

    Admin Model and controller updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,13 @@
 const asyncHandler = require('express-async-handler');
+const Admin = require('../models/admin.model');
 
 const adminLogin = asyncHandler(async(req, res) => {
     // console.log('req.body', req.body);
     const { email, password } = req.body;
+
+    const admin = await Admin.findOne({ email }).select('+password');
+    console.log('admin', admin);
+    
     res.send('Hello from API');
 });
 
