00a74e81d13aafac08d37ae945617fbd8585153d
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-25 22:59:20 +0200

    response util function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,20 @@
 const asyncHandler = require('express-async-handler');
 const Admin = require('../models/admin.model');
+const { responseReture } = require('../utiles/response.util');
 
 const adminLogin = asyncHandler(async(req, res) => {
     // console.log('req.body', req.body);
     const { email, password } = req.body;
 
-    const admin = await Admin.findOne({ email }).select('+password');
-    console.log('admin', admin);
-    
+    try {
+        const admin = await Admin.findOne({ email }).select('+password');
+        console.log('admin', admin);
+
+        if (admin) {} else {};
+    } catch (err) {
+        responseReture(res, 500, { error: error.message });
+    };
+
     res.send('Hello from API');
 });
 
