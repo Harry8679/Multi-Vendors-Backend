ea7b55feefb5ea7332770a7a8bd20eed32f6f550
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-25 23:58:19 +0200

    Register User Route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { adminLogin, loginUser } = require('../controllers/auth.controller');
+const { adminLogin, loginUser, registerUser } = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/admin-login', adminLogin);
 router.post('/login', loginUser);
+router.post('/register', registerUser);
 
 module.exports = router;
